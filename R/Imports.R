b20c5c24d31d3ddb7bad7ee37ef2b0b029c18a54
rvaleriya <valeriya.rogovchenko@gmail.com>
2025-02-11 12:42:12 -0600

    Add main DP-RST function as it is now

--- R/Imports.R
+++ R/Imports.R
@@ -1,4 +1,12 @@
 #' @importFrom fdaPDE create.mesh.2D
 #' @import igraph
 #' @import ggplot2
+#' @importFrom stats runif
+#' @importFrom mvtnorm dmvnorm
+#' @importFrom extraDistr rcat
+#' @importFrom dplyr dense_rank
+#' @importFrom matrixNormal rmatnorm
+#' @importFrom MCMCpack riwish
+#' @importFrom Rfast Table
+#' @importFrom MASS ginv
 NULL
